9e99a736f574501396619d349d11b955d8215600
Yetunde495 <morenikejiy48@gmail.com>
2025-10-01 06:57:16 +0100

    fix: page overflow

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import { Footer } from "@/components/footer";
 import Hero from "@/components/hero-section";
 import ToolsSection from "@/components/tools-section";
 import Image from "next/image";
-
+import { Github, Linkedin, Mail, MousePointerClickIcon } from "lucide-react";
 
 export default function Home() {
   return (
@@ -21,19 +21,34 @@ export default function Home() {
             <div className="w-full lg:max-w-3xl max-sm:mt-[90px] max-lg:mt-[7%]">
               <Hero />
             </div>
-            <div className="w-full flex justify-end">
-              <Image 
-               src="/images/hero-img.png"
-               width={600}
-               height={400}
-               alt="hero-image"
-               className="lg:scale-90 drop-shadow-2xl"
+            <div className="w-full flex flex-col relative gap-3 justify-end">
+              <Image
+                src="/images/hero-img.png"
+                width={600}
+                height={400}
+                alt="hero-image"
+                className="lg:scale-90 drop-shadow-2xl"
               />
+              <div className="w-full -mt-6 sm:ml-[4%]  sm:max-w-[75%] lg:max-w-[400px] bg-slate-50 dark:bg-[#713cbc] shadow-xl border dark:border-none dark:border-t border-slate-200 flex items-center justify-between gap-1 px-4 py-1.5 rounded-xl mb-3">
+                <button className="hover:scale-105 dark:text-white duration-150 rounded-md flex justify-center items-center ">
+                  <Github className="" />
+                </button>
+                <button className="hover:scale-105 dark:text-white duration-150  flex justify-center items-center ">
+                  <Linkedin className="" />
+                </button>
+                <button className=" dark:text-white hover:scale-105 duration-150 rounded-md flex justify-center items-center ">
+                  <Mail className="" />
+                </button>
+
+                <button className=" bg-[#713cbc] text-white p-2 border dark:border-none rounded-md flex justify-center items-center gap-2 max-w-[200px] ">
+                  <MousePointerClickIcon className="h-5 w-5 scale-x-[-1]" />
+                </button>
+              </div>
             </div>
           </div>
         </div>
         {/* <ToolsSection /> */}
-         {/* <BentoDemo /> */}
+        {/* <BentoDemo /> */}
         <BentoGrid />
         <ProjectsSection />
         <Process />
--- components/ui/parallax-cards.tsx
+++ components/ui/parallax-cards.tsx
@@ -16,12 +16,12 @@ export default function ParallaxCards({
   cards,
 }: ParallaxCardsProps) {
   return (
-    <section className="relative w-full min-h-screen overflow-x-hidden">
-      <div  className="relative min-h-[90vh] overflow-x-hidden">
+    <section className="relative w-full min-h-screen">
+      <div  className="relative min-h-[90vh]">
         {cards?.map((card, index) => (
-          <div key={index} className="sticky top-0 py-[7%] min-h-[70vh] z-[1] overflow-x-hidden">
+          <div key={index} className="sticky top-0 py-[7%] min-h-[70vh] z-[1]">
             <div
-              className={`w-full h-full flex items-center  justify-center overflow-x-hidden p-8 max-sm:px-1 rounded-none bg-[#f8f7f7] dark:bg-[#030711]`}
+              className={`w-full h-full flex items-center  overflow-x-hidden  justify-center p-8 max-sm:px-1 rounded-none bg-[#f8f7f7] dark:bg-[#030711]`}
             >
               {card.content}
             </div>
