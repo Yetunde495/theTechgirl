e578c437253c7b18163a331fcc68aa2b703dd728
Yetunde495 <morenikejiy48@gmail.com>
2025-10-01 06:13:46 +0100

    -Add Hero Image

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
 import { Navigation } from "@/components/navigation";
 import { BentoGrid } from "@/components/bento-grid";
 import { ProjectsSection } from "@/components/projects-section";
-import { Process, ProcessTimeline } from "@/components/process-timeline";
+import { Process } from "@/components/process-timeline";
 import { TestimonialsSection } from "@/components/testimonials-section";
 import { ValueProposition } from "@/components/value-proposition";
 import { CTASection } from "@/components/cta-section";
 import { Footer } from "@/components/footer";
 import Hero from "@/components/hero-section";
-import { AnimatedProfile } from "@/components/animated-card";
-import { DynamicIslandProvider } from "@/components/ui/dynamic-island";
 import ToolsSection from "@/components/tools-section";
+import Image from "next/image";
 
 
 export default function Home() {
@@ -18,16 +17,23 @@ export default function Home() {
       <main className="">
         <Navigation />
         <div className="hero-section">
-          <div className="container mx-auto px-4 py-[7%] gap-y-16 gap-x-5 flex max-lg:flex-col justify-center lg:items-center h-full min-h-[98vh]">
+          <div className="container mx-auto px-4 py-[7%] gap-y-16 gap-x-5 flex max-lg:flex-col justify-center items-center h-full min-h-[98vh]">
             <div className="w-full lg:max-w-3xl max-sm:mt-[90px] max-lg:mt-[7%]">
               <Hero />
             </div>
-            <div className="">
-              <DynamicIslandProvider initialSize={"default"}>
+            <div className="w-full flex justify-end">
+              {/* <DynamicIslandProvider initialSize={"default"}>
                 <div>
                   <AnimatedProfile />
                 </div>
-              </DynamicIslandProvider>
+              </DynamicIslandProvider> */}
+              <Image 
+               src="/images/hero-img.png"
+               width={600}
+               height={400}
+               alt="hero-image"
+               className="lg:scale-90 drop-shadow-2xl"
+              />
             </div>
           </div>
         </div>
