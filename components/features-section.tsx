7c608d142c7891f26761b386c8f6bbd927978a10
Yetunde495 <morenikejiy48@gmail.com>
2025-10-14 16:46:04 +0100

    fix: oveflow

--- components/features-section.tsx
+++ components/features-section.tsx
@@ -117,7 +117,7 @@ export const FeaturesSection = () => {
           </div>
         </motion.div>
         {/* Bottom Row - Adapted to match image */}
-      <div className="mt-4  md:col-span-3 flex flex-col md:flex-row items-center justify-between bg-white dark:bg-gray-900/70 rounded-md p-6 shadow-sm space-y-4 md:space-y-0 md:space-x-4">
+      <div className="mt-4  md:col-span-3 hidden flex-col md:flex-row items-center justify-between bg-white dark:bg-gray-900/70 rounded-md p-6 shadow-sm space-y-4 md:space-y-0 md:space-x-4">
         <button className="bg-primary text-white rounded-full px-4 py-2 flex items-center">
           <svg
             xmlns="http://www.w3.org/2000/svg"
